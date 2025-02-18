a3acfa0da467335fe85068956e9bc6aea6eb1dd9
unknown <mahsuma.rezai8@gmail.com>
2025-02-17 23:09:40 -0800

    work on app projects

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { Fragment } from 'react';
+
+
+
+const App = (props) => {
+    return (
+        <Fragment>
+
+            <h1>hello </h1>
+
+
+
+
+
+        </Fragment>
+    );
+};
+
+export default App;
