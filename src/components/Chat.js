01595881b3b5fb157e78a9a9c2f761953e4355f8
unknown <mahsuma.rezai8@gmail.com>
2025-02-20 10:11:55 -0800

    work on btns

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,36 +2,38 @@ import React, { useState, useEffect } from "react";
 import "../styles/Footer.css";
 
 const Chats = (props) => {
-  const [searchHistory, setSearchHistory] = useState([]);
+    const [searchHistory, setSearchHistory] = useState([]);
 
-  useEffect(() => {
-    const history = JSON.parse(localStorage.getItem("searchHistory")) || [];
-    setSearchHistory(history);
-  }, []);
+    useEffect(() => {
+        const history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+        setSearchHistory(history);
+    }, []);
+    const clearSearchHistory = () => {
+        localStorage.removeItem("searchHistory"); 
+        setSearchHistory([]); 
+    };
 
-  // تابع برای پاک کردن تاریخچه جستجو
-  const clearSearchHistory = () => {
-    localStorage.removeItem("searchHistory"); // یا برای پاک کردن کامل localStorage.clear()
-    setSearchHistory([]); // به روزرسانی وضعیت برای حذف آیتم‌ها از رابط کاربری
-  };
+    return (
+        <div className="Chats">
+            <h2>Search History</h2>
+            {searchHistory.length > 0 ? (
+                <ul>
+                    {searchHistory.map((word, index) => (
+                        <li key={index}>{word}</li>
+                    ))}
+                </ul>
+            ) : (
+                    <p>No search history available.</p>
+                )}
+            <div className="button">
+                <button className="btn" onClick={props.onClose}>Close</button>
+                <button className="btn-remve" onClick={clearSearchHistory}>Clear History</button>
+            </div>
 
-  return (
-    <div className="Chats">
-      <h2>Search History</h2>
-      {searchHistory.length > 0 ? (
-        <ul>
-          {searchHistory.map((word, index) => (
-            <li key={index}>{word}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No search history available.</p>
-      )}
-      <button className="btn" onClick={props.onClose}>Close</button>
-      {/* دکمه جدید برای پاک کردن تاریخچه جستجو */}
-      <button className="btn" onClick={clearSearchHistory}>Clear History</button>
-    </div>
-  );
+
+
+        </div>
+    );
 };
 
 export default Chats;
