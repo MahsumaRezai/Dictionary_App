fa16527bbb7f9696ec1e8bb3ef7a12e15ce05aa7
unknown <mahsuma.rezai8@gmail.com>
2025-02-20 09:30:30 -0800

    work on the clear chat section

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,28 +2,36 @@ import React, { useState, useEffect } from "react";
 import "../styles/Footer.css";
 
 const Chats = (props) => {
-    const [searchHistory, setSearchHistory] = useState([]);
+  const [searchHistory, setSearchHistory] = useState([]);
 
-    useEffect(() => {
-        const history = JSON.parse(localStorage.getItem("searchHistory")) || [];
-        setSearchHistory(history);
-    }, []);
+  useEffect(() => {
+    const history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    setSearchHistory(history);
+  }, []);
 
-    return (
-        <div className="Chats">
-            <h2>Search History</h2>
-            {searchHistory.length > 0 ? (
-                <ul>
-                    {searchHistory.map((word, index) => (
-                        <li key={index}>{word}</li>
-                    ))}
-                </ul>
-            ) : (
-                    <p>No search history available.</p>
-                )}
-            <button className="btn" onClick={props.onClose}>close</button>
-        </div>
-    );
+  // تابع برای پاک کردن تاریخچه جستجو
+  const clearSearchHistory = () => {
+    localStorage.removeItem("searchHistory"); // یا برای پاک کردن کامل localStorage.clear()
+    setSearchHistory([]); // به روزرسانی وضعیت برای حذف آیتم‌ها از رابط کاربری
+  };
+
+  return (
+    <div className="Chats">
+      <h2>Search History</h2>
+      {searchHistory.length > 0 ? (
+        <ul>
+          {searchHistory.map((word, index) => (
+            <li key={index}>{word}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No search history available.</p>
+      )}
+      <button className="btn" onClick={props.onClose}>Close</button>
+      {/* دکمه جدید برای پاک کردن تاریخچه جستجو */}
+      <button className="btn" onClick={clearSearchHistory}>Clear History</button>
+    </div>
+  );
 };
 
 export default Chats;
